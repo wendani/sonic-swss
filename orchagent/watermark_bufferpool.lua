7c92e5959adf0a144751fd845043bd14c6d856a6
Wenda Ni <wenni@microsoft.com>
2019-04-23 06:15:33 +0000

    Fix syntax error in buffer pool watermark lua script
    Signed-off-by: Wenda Ni <wenni@microsoft.com>

--- orchagent/watermark_bufferpool.lua
+++ orchagent/watermark_bufferpool.lua
@@ -35,6 +35,7 @@ for i = n, 1, -1 do
             user_wm_last = tonumber(user_wm_last)
             if wm > user_wm_last then
                 redis.call('HSET', user_table_name .. ':' .. KEYS[i], sai_buffer_pool_watermark_stat_name, wm)
+            end
         else
             redis.call('HSET', user_table_name .. ':' .. KEYS[i], sai_buffer_pool_watermark_stat_name, wm)
         end
@@ -43,6 +44,7 @@ for i = n, 1, -1 do
             persistent_wm_last = tonumber(persistent_wm_last)
             if wm > persistent_wm_last then
                 redis.call('HSET', persistent_table_name .. ':' .. KEYS[i], sai_buffer_pool_watermark_stat_name, wm)
+            end
         else
             redis.call('HSET', persistent_table_name .. ':' .. KEYS[i], sai_buffer_pool_watermark_stat_name, wm)
         end
@@ -51,6 +53,7 @@ for i = n, 1, -1 do
             periodic_wm_last = tonumber(periodic_wm_last)
             if wm > periodic_wm_last then
                 redis.call('HSET', periodic_table_name .. ':' .. KEYS[i], sai_buffer_pool_watermark_stat_name, wm)
+            end
         else
             redis.call('HSET', periodic_table_name .. ':' .. KEYS[i], sai_buffer_pool_watermark_stat_name, wm)
         end
