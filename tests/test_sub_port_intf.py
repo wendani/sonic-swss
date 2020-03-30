add24e3c3b18ac6a0f220d559c1f0ab107de1903
Wenda Ni <wenda.ni@bytedance.com>
2020-03-30 16:53:05 +0800

    Test creating a lag sub interface
    Signed-off-by: Wenda Ni <wonda.ni@gmail.com>

--- tests/test_sub_port_intf.py
+++ tests/test_sub_port_intf.py
@@ -3,11 +3,15 @@ import time
 import json
 
 from swsscommon import swsscommon
+import sys
 
 CFG_VLAN_SUB_INTF_TABLE_NAME = "VLAN_SUB_INTERFACE"
 CFG_PORT_TABLE_NAME = "PORT"
+CFG_LAG_TABLE_NAME = "PORTCHANNEL"
+CFG_LAG_MEMBER_TABLE_NAME = "PORTCHANNEL_MEMBER"
 
 STATE_PORT_TABLE_NAME = "PORT_TABLE"
+STATE_LAG_TABLE_NAME = "LAG_TABLE"
 STATE_INTERFACE_TABLE_NAME = "INTERFACE_TABLE"
 
 APP_INTF_TABLE_NAME = "INTF_TABLE"
@@ -17,10 +21,18 @@ ASIC_ROUTE_ENTRY_TABLE = "ASIC_STATE:SAI_OBJECT_TYPE_ROUTE_ENTRY"
 
 ADMIN_STATUS = "admin_status"
 
+ETHERNET_PREFIX = "Ethernet"
+LAG_PREFIX = "PortChannel"
+
+VLAN_SUB_INTERFACE_SEPARATOR = "."
+
 
 class TestSubPortIntf(object):
-    PHYSICAL_PORT_UNDER_TEST = "Ethernet64"
     SUB_PORT_INTERFACE_UNDER_TEST = "Ethernet64.10"
+    PHYSICAL_PORT_UNDER_TEST = SUB_PORT_INTERFACE_UNDER_TEST.split('.')[0]
+
+    LAG_SUB_PORT_INTERFACE_UNDER_TEST = "PortChannel1.20"
+    LAG_UNDER_TEST = LAG_SUB_PORT_INTERFACE_UNDER_TEST.split('.')[0]
 
     IPV4_ADDR_UNDER_TEST = "10.0.0.33/31"
     IPV4_TOME_UNDER_TEST = "10.0.0.33/32"
@@ -39,10 +51,20 @@ class TestSubPortIntf(object):
     def set_parent_port_admin_status(self, port_name, status):
         fvs = swsscommon.FieldValuePairs([(ADMIN_STATUS, status)])
 
-        tbl = swsscommon.Table(self.config_db, CFG_PORT_TABLE_NAME)
+        tbl_name = CFG_PORT_TABLE_NAME
+        if port_name.startswith('PortChannel'):
+            tbl_name = CFG_LAG_TABLE_NAME
+        else:
+            assert port_name.startswith('Ethernet')
+        tbl = swsscommon.Table(self.config_db, tbl_name)
         tbl.set(port_name, fvs)
-
         time.sleep(1)
+
+        #if tbl_name == CFG_LAG_MEMBER_TABLE_NAME
+        #    tbl = swsscommon.Table(self.config_db, CFG_LAG_MEMBER_TABLE_NAME)
+        #    fvs = swsscommon.FieldValuePairs([("NULL", "NULL")])
+        #    tbl.set("{}|Ethernet4".format(port_name), fvs)
+        #    time.sleep(1)
 
     def create_sub_port_intf_profile(self, sub_port_intf_name):
         fvs = swsscommon.FieldValuePairs([(ADMIN_STATUS, "up")])
@@ -146,30 +168,37 @@ class TestSubPortIntf(object):
             route_entry = json.loads(raw_route_entry)
             assert route_entry["dest"] not in removed_route_entries
 
-    def test_sub_port_intf_creation(self, dvs):
-        self.connect_dbs(dvs)
+    def _test_sub_port_intf_creation(self, dvs, sub_port_intf_name):
+        substrs = sub_port_intf_name.split(VLAN_SUB_INTERFACE_SEPARATOR)
+        parent_port = substrs[0]
+        vlan_id = substrs[1]
+        if parent_port.startswith(ETHERNET_PREFIX):
+            state_tbl_name = STATE_PORT_TABLE_NAME
+        else:
+            assert parent_port.startswith(LAG_PREFIX)
+            state_tbl_name = STATE_LAG_TABLE_NAME
 
         old_rif_oids = self.get_oids(ASIC_RIF_TABLE)
 
-        self.set_parent_port_admin_status(self.PHYSICAL_PORT_UNDER_TEST, "up")
-        self.create_sub_port_intf_profile(self.SUB_PORT_INTERFACE_UNDER_TEST)
+        self.set_parent_port_admin_status(parent_port, "up")
+        self.create_sub_port_intf_profile(sub_port_intf_name)
 
         # Verify that sub port interface state ok is pushed to STATE_DB by Intfmgrd
         fv_dict = {
             "state": "ok",
         }
-        self.check_sub_port_intf_fvs(self.state_db, STATE_PORT_TABLE_NAME, self.SUB_PORT_INTERFACE_UNDER_TEST, fv_dict)
+        self.check_sub_port_intf_fvs(self.state_db, state_tbl_name, sub_port_intf_name, fv_dict)
 
         # Verify that sub port interface configuration is synced to APPL_DB INTF_TABLE by Intfmgrd
         fv_dict = {
             ADMIN_STATUS: "up",
         }
-        self.check_sub_port_intf_fvs(self.appl_db, APP_INTF_TABLE_NAME, self.SUB_PORT_INTERFACE_UNDER_TEST, fv_dict)
+        self.check_sub_port_intf_fvs(self.appl_db, APP_INTF_TABLE_NAME, sub_port_intf_name, fv_dict)
 
         # Verify that a sub port router interface entry is created in ASIC_DB
         fv_dict = {
             "SAI_ROUTER_INTERFACE_ATTR_TYPE": "SAI_ROUTER_INTERFACE_TYPE_SUB_PORT",
-            "SAI_ROUTER_INTERFACE_ATTR_OUTER_VLAN_ID": "10",
+            "SAI_ROUTER_INTERFACE_ATTR_OUTER_VLAN_ID": "{}".format(vlan_id),
             "SAI_ROUTER_INTERFACE_ATTR_ADMIN_V4_STATE": "true",
             "SAI_ROUTER_INTERFACE_ATTR_ADMIN_V6_STATE": "true",
             "SAI_ROUTER_INTERFACE_ATTR_MTU": "9100",
@@ -178,7 +207,13 @@ class TestSubPortIntf(object):
         self.check_sub_port_intf_fvs(self.asic_db, ASIC_RIF_TABLE, rif_oid, fv_dict)
 
         # Remove a sub port interface
-        self.remove_sub_port_intf_profile(self.SUB_PORT_INTERFACE_UNDER_TEST)
+        self.remove_sub_port_intf_profile(sub_port_intf_name)
+
+    def test_sub_port_intf_creation(self, dvs):
+        self.connect_dbs(dvs)
+
+        self._test_sub_port_intf_creation(dvs, self.SUB_PORT_INTERFACE_UNDER_TEST)
+        self._test_sub_port_intf_creation(dvs, self.LAG_SUB_PORT_INTERFACE_UNDER_TEST)
 
     def test_sub_port_intf_add_ip_addrs(self, dvs):
         self.connect_dbs(dvs)
