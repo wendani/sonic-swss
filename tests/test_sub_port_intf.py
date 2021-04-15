822b1a11c900a9136639df0c0b9f31650403ecf5
Wenda Ni <wonda.ni@gmail.com>
2021-04-15 00:55:38 +0000

    removal sequence test
    Signed-off-by: Wenda Ni <wonda.ni@gmail.com>

--- tests/test_sub_port_intf.py
+++ tests/test_sub_port_intf.py
@@ -673,9 +673,10 @@ class TestSubPortIntf(object):
         self._test_sub_port_intf_remove_ip_addrs(dvs, self.SUB_PORT_INTERFACE_UNDER_TEST, self.VRF_UNDER_TEST)
         self._test_sub_port_intf_remove_ip_addrs(dvs, self.LAG_SUB_PORT_INTERFACE_UNDER_TEST, self.VRF_UNDER_TEST)
 
-    def _test_sub_port_intf_removal(self, dvs, sub_port_intf_name, vrf_name=None):
+    def _test_sub_port_intf_removal(self, dvs, sub_port_intf_name, removal_seq_test=False, vrf_name=None):
         substrs = sub_port_intf_name.split(VLAN_SUB_INTERFACE_SEPARATOR)
         parent_port = substrs[0]
+        vlan_id = substrs[1]
         if parent_port.startswith(ETHERNET_PREFIX):
             state_tbl_name = STATE_PORT_TABLE_NAME
             phy_ports = [parent_port]
@@ -684,6 +685,7 @@ class TestSubPortIntf(object):
             state_tbl_name = STATE_LAG_TABLE_NAME
             phy_ports = self.LAG_MEMBERS_UNDER_TEST
 
+        vrf_oid = self.default_vrf_oid
         old_rif_oids = self.get_oids(ASIC_RIF_TABLE)
 
         self.set_parent_port_admin_status(dvs, parent_port, "up")
@@ -693,13 +695,11 @@ class TestSubPortIntf(object):
             self.asic_db.wait_for_n_keys(ASIC_LAG_MEMBER_TABLE, len(self.LAG_MEMBERS_UNDER_TEST))
         if vrf_name:
             self.create_vrf(vrf_name)
-            self.asic_db.wait_for_n_keys(ASIC_VIRTUAL_ROUTER_TABLE, 2)
+            vrf_oid = self.get_newly_created_oid(ASIC_VIRTUAL_ROUTER_TABLE, [vrf_oid])
         self.create_sub_port_intf_profile(sub_port_intf_name, vrf_name)
 
         self.add_sub_port_intf_ip_addr(sub_port_intf_name, self.IPV4_ADDR_UNDER_TEST)
         self.add_sub_port_intf_ip_addr(sub_port_intf_name, self.IPV6_ADDR_UNDER_TEST)
-
-        rif_oid = self.get_newly_created_oid(ASIC_RIF_TABLE, old_rif_oids)
 
         fv_dict = {
             "state": "ok",
@@ -722,6 +722,48 @@ class TestSubPortIntf(object):
             fv_dict[VRF_NAME] = vrf_name
         self.check_sub_port_intf_fvs(self.app_db, APP_INTF_TABLE_NAME, sub_port_intf_name, fv_dict)
 
+        if removal_seq_test == True:
+            # Remove a sub port interface before removing sub port interface IP addresses
+            self.remove_sub_port_intf_profile(sub_port_intf_name)
+            time.sleep(2)
+
+            # Verify that sub port interface state ok persists in STATE_DB
+            fv_dict = {
+                "state": "ok",
+            }
+            self.check_sub_port_intf_fvs(self.state_db, state_tbl_name, sub_port_intf_name, fv_dict)
+            # Verify vrf name sub port interface bound to persists in STATE_DB INTERFACE_TABLE
+            fv_dict = {
+                "vrf": vrf_name if vrf_name else "",
+            }
+            self.check_sub_port_intf_fvs(self.state_db, STATE_INTERFACE_TABLE_NAME, sub_port_intf_name, fv_dict)
+            # If bound to non-default vrf, verify sub port interface vrf binding in linux kernel,
+            # and parent port not bound to vrf
+            if vrf_name:
+                self.check_sub_port_intf_vrf_bind_kernel(dvs, sub_port_intf_name, vrf_name)
+                self.check_sub_port_intf_vrf_nobind_kernel(dvs, parent_port, vrf_name)
+            # Verify that sub port interface configuration persists in APPL_DB INTF_TABLE
+            fv_dict = {
+                ADMIN_STATUS: "up",
+            }
+            if vrf_name:
+                fv_dict[VRF_NAME] = vrf_name
+            self.check_sub_port_intf_fvs(self.app_db, APP_INTF_TABLE_NAME, sub_port_intf_name, fv_dict)
+
+            # Verify that a sub port router interface entry persists in ASIC_DB
+            fv_dict = {
+                "SAI_ROUTER_INTERFACE_ATTR_TYPE": "SAI_ROUTER_INTERFACE_TYPE_SUB_PORT",
+                "SAI_ROUTER_INTERFACE_ATTR_OUTER_VLAN_ID": "{}".format(vlan_id),
+                "SAI_ROUTER_INTERFACE_ATTR_ADMIN_V4_STATE": "true",
+                "SAI_ROUTER_INTERFACE_ATTR_ADMIN_V6_STATE": "true",
+                "SAI_ROUTER_INTERFACE_ATTR_MTU": DEFAULT_MTU,
+                "SAI_ROUTER_INTERFACE_ATTR_VIRTUAL_ROUTER_ID": vrf_oid,
+            }
+            rif_oid = self.get_newly_created_oid(ASIC_RIF_TABLE, old_rif_oids)
+            self.check_sub_port_intf_fvs(self.asic_db, ASIC_RIF_TABLE, rif_oid, fv_dict)
+        else:
+            rif_oid = self.get_newly_created_oid(ASIC_RIF_TABLE, old_rif_oids)
+
         # Remove IP addresses
         self.remove_sub_port_intf_ip_addr(sub_port_intf_name, self.IPV4_ADDR_UNDER_TEST)
         self.remove_sub_port_intf_ip_addr(sub_port_intf_name, self.IPV6_ADDR_UNDER_TEST)
@@ -729,8 +771,9 @@ class TestSubPortIntf(object):
                                                  [self.IPV4_ADDR_UNDER_TEST,
                                                   self.IPV6_ADDR_UNDER_TEST])
 
-        # Remove a sub port interface
-        self.remove_sub_port_intf_profile(sub_port_intf_name)
+        if removal_seq_test == False:
+            # Remove a sub port interface
+            self.remove_sub_port_intf_profile(sub_port_intf_name)
         self.check_sub_port_intf_profile_removal(rif_oid)
 
         # Verify that sub port interface state ok is removed from STATE_DB by Intfmgrd
@@ -775,8 +818,14 @@ class TestSubPortIntf(object):
         self._test_sub_port_intf_removal(dvs, self.SUB_PORT_INTERFACE_UNDER_TEST)
         self._test_sub_port_intf_removal(dvs, self.LAG_SUB_PORT_INTERFACE_UNDER_TEST)
 
-        self._test_sub_port_intf_removal(dvs, self.SUB_PORT_INTERFACE_UNDER_TEST, self.VRF_UNDER_TEST)
-        self._test_sub_port_intf_removal(dvs, self.LAG_SUB_PORT_INTERFACE_UNDER_TEST, self.VRF_UNDER_TEST)
+        self._test_sub_port_intf_removal(dvs, self.SUB_PORT_INTERFACE_UNDER_TEST, vrf_name=self.VRF_UNDER_TEST)
+        self._test_sub_port_intf_removal(dvs, self.LAG_SUB_PORT_INTERFACE_UNDER_TEST, vrf_name=self.VRF_UNDER_TEST)
+
+        self._test_sub_port_intf_removal(dvs, self.SUB_PORT_INTERFACE_UNDER_TEST, removal_seq_test=True)
+        self._test_sub_port_intf_removal(dvs, self.LAG_SUB_PORT_INTERFACE_UNDER_TEST, removal_seq_test=True)
+
+        self._test_sub_port_intf_removal(dvs, self.SUB_PORT_INTERFACE_UNDER_TEST, removal_seq_test=True, vrf_name=self.VRF_UNDER_TEST)
+        self._test_sub_port_intf_removal(dvs, self.LAG_SUB_PORT_INTERFACE_UNDER_TEST, removal_seq_test=True, vrf_name=self.VRF_UNDER_TEST)
 
     def _test_sub_port_intf_mtu(self, dvs, sub_port_intf_name, vrf_name=None):
         substrs = sub_port_intf_name.split(VLAN_SUB_INTERFACE_SEPARATOR)
