73429b19ed4c448895f548c2235de6fb49c3e1da
Wenda Ni <wonda.ni@gmail.com>
2021-07-19 20:56:23 +0000

    Fix space in syslog message
    Signed-off-by: Wenda Ni <wonda.ni@gmail.com>

--- orchagent/fdborch.cpp
+++ orchagent/fdborch.cpp
@@ -73,8 +73,8 @@ bool FdbOrch::storeFdbEntryState(const FdbUpdate& update)
 
     if (!m_portsOrch->getPort(entry.bv_id, vlan))
     {
-        SWSS_LOG_NOTICE("FdbOrch notification: Failed to locate \
-                         vlan port from bv_id 0x%" PRIx64, entry.bv_id);
+        SWSS_LOG_NOTICE("FdbOrch notification: Failed to locate "
+                        "vlan port from bv_id 0x%" PRIx64, entry.bv_id);
         return false;
     }
 
