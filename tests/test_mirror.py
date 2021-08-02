52f0777cd2bdbd344d306c7f0a0861e352a47e8b
Wenda Ni <wonda.ni@gmail.com>
2021-08-02 15:17:39 +0000

    Sleep before unchanged monitor port check
    Signed-off-by: Wenda Ni <wonda.ni@gmail.com>

--- tests/test_mirror.py
+++ tests/test_mirror.py
@@ -601,6 +601,7 @@ class TestMirror(object):
         # add a new member to lag
         self.set_interface_status(dvs, "Ethernet92", "up")
         self.create_port_channel_member("008", "Ethernet92", status="enabled")
+        time.sleep(2)
         # monitor port stays unchanged
         assert self.get_mirror_session_state(session)["status"] == "active"
         assert self.get_mirror_session_state(session)["monitor_port"] == "Ethernet88"
@@ -630,6 +631,7 @@ class TestMirror(object):
 
         # Restore lag member oper status up
         self.create_port_channel_member("008", "Ethernet88", status="enabled")
+        time.sleep(2)
         # monitor port stays unchanged
         assert self.get_mirror_session_state(session)["status"] == "active"
         assert self.get_mirror_session_state(session)["monitor_port"] == "Ethernet92"
