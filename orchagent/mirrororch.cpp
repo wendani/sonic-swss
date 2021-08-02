467daaeaa78871bed0dba27847138fe3c7f76349
Wenda Ni <wonda.ni@gmail.com>
2021-08-02 08:08:52 +0000

    Rename variable
    Signed-off-by: Wenda Ni <wonda.ni@gmail.com>

--- orchagent/mirrororch.cpp
+++ orchagent/mirrororch.cpp
@@ -1423,10 +1423,10 @@ void MirrorOrch::updateLagMember(const LagMemberUpdate& update)
         {
             if (session.status && session.neighborInfo.portId == update.member.m_port_id)
             {
-                Port p;
-                if (selectEnabledLagMember(update.lag, p))
+                Port member;
+                if (selectEnabledLagMember(update.lag, member))
                 {
-                    session.neighborInfo.portId = p.m_port_id;
+                    session.neighborInfo.portId = member.m_port_id;
                     updateSessionDstPort(name, session);
                 }
                 else
@@ -1469,10 +1469,10 @@ void MirrorOrch::updateLagMemberStatus(const LagMemberStatusUpdate& update)
         {
             if (session.status && session.neighborInfo.portId == update.member.m_port_id)
             {
-                Port p;
-                if (selectEnabledLagMember(update.lag, p))
+                Port member;
+                if (selectEnabledLagMember(update.lag, member))
                 {
-                    session.neighborInfo.portId = p.m_port_id;
+                    session.neighborInfo.portId = member.m_port_id;
                     updateSessionDstPort(name, session);
                 }
                 else
