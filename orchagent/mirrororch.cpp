f9529a8eefd670a5d30f7445944fa42ffa9ed604
Wenda Ni <wonda.ni@gmail.com>
2021-08-04 20:04:13 +0000

    Rename variable
    Signed-off-by: Wenda Ni <wonda.ni@gmail.com>

--- orchagent/mirrororch.cpp
+++ orchagent/mirrororch.cpp
@@ -624,14 +624,14 @@ bool MirrorOrch::getNeighborInfo(const string& name, MirrorEntry& session)
             }
             else
             {
-                Port member;
-                if (!selectEnabledLagMember(session.neighborInfo.port, member))
+                Port lmp;
+                if (!selectEnabledLagMember(session.neighborInfo.port, lmp))
                 {
                     session.neighborInfo.portId = SAI_NULL_OBJECT_ID;
                     return false;
                 }
 
-                session.neighborInfo.portId = member.m_port_id;
+                session.neighborInfo.portId = lmp.m_port_id;
             }
 
             return true;
@@ -1423,10 +1423,10 @@ void MirrorOrch::updateLagMember(const LagMemberUpdate& update)
         {
             if (session.status && session.neighborInfo.portId == update.member.m_port_id)
             {
-                Port member;
-                if (selectEnabledLagMember(update.lag, member))
+                Port lmp;
+                if (selectEnabledLagMember(update.lag, lmp))
                 {
-                    session.neighborInfo.portId = member.m_port_id;
+                    session.neighborInfo.portId = lmp.m_port_id;
                     updateSessionDstPort(name, session);
                 }
                 else
@@ -1469,10 +1469,10 @@ void MirrorOrch::updateLagMemberStatus(const LagMemberStatusUpdate& update)
         {
             if (session.status && session.neighborInfo.portId == update.member.m_port_id)
             {
-                Port member;
-                if (selectEnabledLagMember(update.lag, member))
+                Port lmp;
+                if (selectEnabledLagMember(update.lag, lmp))
                 {
-                    session.neighborInfo.portId = member.m_port_id;
+                    session.neighborInfo.portId = lmp.m_port_id;
                     updateSessionDstPort(name, session);
                 }
                 else
